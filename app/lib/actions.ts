daf9573ad5ce7e9ae8dfd408736e4da59f587a66
hamsoool <hanslacuesta@gmail.com>
2025-10-22 12:24:53 +0800

    may error di ko mahanap huhu

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,11 @@
 'use server';
 
 import { z } from 'zod';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import postgres from 'postgres';
+
+const sql = postgres(process.env.POSTGRES_URL!, {ssl: 'require'});
 
 const FormSchema = z.object({
     id: z.string(),
@@ -20,6 +25,36 @@ export async function createInvoice(formData: FormData) {
     status: formData.get('status'),
   });
     const amountInCents = amount * 100;
-    const date = new Date().toISOString(). spit('T')[0];
-    }
+    const date = new Date().toISOString(). split('T')[0];
+    await sql` 
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+    
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+}
+    // update
 
+    const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+    export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+  
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+
+}
